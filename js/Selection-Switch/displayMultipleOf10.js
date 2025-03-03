d972febbf8e0b9204d906ccce388528e9db67e84
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-03-04 00:46:11 +0530

    [Sudhakar] Add. Added number to word converter

---
+++ js/Selection-Switch/displayMultipleOf10.js
@@ -0,0 +1,24 @@
+const num = Number(process.argv[2]);
+
+switch (num) {
+  case 1:
+    console.log("Unit");
+    break
+  case 10:
+    console.log("Ten");
+    break
+  case 100:
+    console.log("Hundred");
+    break;
+  case 1000:
+    console.log("Thousand");
+    break
+  case 10000:
+    console.log("Ten Thousand");
+    break
+  case 100000:
+    console.log("Hundred Thousand")
+    break
+  default:
+    console.log("Enter number multiple of 10")
+}
