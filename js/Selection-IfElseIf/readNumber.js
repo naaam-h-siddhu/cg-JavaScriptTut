b61b23f721fdd9a32bcc595b7874a680551c3199
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-03-03 23:59:08 +0530

    [Sudhakar] Add. added program to read number multiple of 10

---
+++ js/Selection-IfElseIf/readNumber.js
@@ -0,0 +1,26 @@
+const num = Number(process.argv[2]);
+
+if(num === 1){
+  console.log("Unit");
+}
+else if(num === 10){
+  console.log("Ten");
+}
+else if(num === 100){
+  console.log("Hundred");
+}
+else if(num === 1000){
+  console.log("Thousand");
+}
+else if(num === 10000){
+  console.log("Ten Thousand");
+}
+else if(num === 100000){
+  console.log("Hundred Thousand");
+}
+else if(num === 1000000){
+  console.log("Thousand Thousand");
+}
+else{
+  console.log("Enter a number multiple of 10 ");
+}
