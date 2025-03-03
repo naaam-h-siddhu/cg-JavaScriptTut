8399f8b5707794a31c04affbbef290f613f812f8
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-03-04 01:27:42 +0530

    [Sudhakar] Add. Added program to check for prime number

---
+++ js/Repetition-ForLoop/isPrime.js
@@ -0,0 +1,13 @@
+const num = Number(process.argv[2]);
+
+let factors = 0;
+for(let i = 2;i<num;i++){
+  if(num%i == 0)
+    factors++;
+}
+if(factors == 0){
+  console.log("Yes its a Prime number");
+}
+else{
+  console.log("No its not a prime number");
+}
