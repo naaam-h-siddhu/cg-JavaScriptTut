f20d5c27a20510998ae7c4068c50f5df051a8495
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-03-04 01:32:50 +0530

    [Sudhakar] Add. extended prime number program to print prime number in given range

--- js/Repetition-ForLoop/isPrime.js
+++ js/Repetition-ForLoop/isPrime.js
@@ -1,13 +1,25 @@
 const num = Number(process.argv[2]);
 
-let factors = 0;
-for(let i = 2;i<num;i++){
-  if(num%i == 0)
+// let factors = 0;
+// for(let i = 2;i<num;i++){
+//   if(num%i == 0)
+//     factors++;
+// }
+// if(factors == 0){
+//   console.log("Yes its a Prime number");
+// }
+// else{
+//   console.log("No its not a prime number");
+// }
+
+for(let i = 1; i <=num;i++) {
+
+  let factors = 0;
+  for(let j = 2;j<i;j++){
+  if(i%j == 0)
     factors++;
-}
-if(factors == 0){
-  console.log("Yes its a Prime number");
-}
-else{
-  console.log("No its not a prime number");
+  }
+  if(factors == 0)
+    console.log(i);
+
 }
